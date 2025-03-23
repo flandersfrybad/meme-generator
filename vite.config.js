6f1e9add0fc0572a7b8ddf3a1e2f9c6f17b5a237
flandersfrybad <152252386+flandersfrybad@users.noreply.github.com>
2025-03-23 01:33:10 -0600

    last

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import {defineConfig} from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-	base: '/meme-generator/',
+	base: '/',
 	build: {
 		outDir: 'dist',
 		assetsDir: 'assets', 
